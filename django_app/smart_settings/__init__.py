b7cc003bd2a12f19c6554ccbcb4873fa05e9a6c3
uchood <uchood@gmail.com>
2016-03-10 23:49:13 +0300

    add version to __init__.py

--- django_app/smart_settings/__init__.py
+++ django_app/smart_settings/__init__.py
@@ -1,1 +1,9 @@
 default_app_config = 'smart_settings.apps.SmartSettingsConfig'
+
+__version__ = '1.0.0'
+__author__ = 'Uchood'
+__license__ = ''
+__copyright__ = 'Copyright 2016 Uchood'
+
+# Version synonym
+VERSION = __version__
